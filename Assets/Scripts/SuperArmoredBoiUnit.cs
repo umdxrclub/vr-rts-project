7a2fa92c69f8a49c3733fc0d52fc6519a7754cd3
hazelfeather17777 <ireneli77@gmail.com>
2019-04-10 18:59:14 -0400

    SuperArmoredBoi has joined the chat!
    Super slow and ridiculously armored.

---
+++ Assets/Scripts/SuperArmoredBoiUnit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperArmoredBoiUnit : Unit
+{
+    void Start()
+    {
+        // Override the default Unit stats
+        speed = 0.4f;
+        maxHealth = 20;
+
+        setUpUnit();
+    }
+}
